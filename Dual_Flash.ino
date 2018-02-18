610f4a0e6f3910a112e9ad46f3eca22e6028247f
Ebram Michel <36461637+BeroBero16@users.noreply.github.com>
2018-02-18 02:53:14 +0200

    Dual_Flash

---
+++ Dual_Flash.ino
@@ -0,0 +1,14 @@
+const byte led1 = 1;
+const byte led2 = 2;
+void setup() {
+pinMode ( led1 , OUTPUT );
+pinMode ( led2 , OUTPUT ) ;
+}
+void loop() {
+ digitalWrite ( led1 , HIGH );
+ delay ( 1000 );
+ digitalWrite ( led1 , LOW );
+ digitalWrite ( led2 , HIGH );
+ delay ( 1000 );
+ digitalWrite ( led2 , LOW );
+}
