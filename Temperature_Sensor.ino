36ee2e73bb4313afaef4cf7b7c8f7dde522e5728
Ebram Michel <36461637+BeroBero16@users.noreply.github.com>
2018-02-18 03:17:07 +0200

    Temperature Sensor

---
+++ Temperature_Sensor.ino
@@ -0,0 +1,12 @@
+const byte sensor = 1;
+const byte led = 2;
+void setup() {
+pinMode ( sensor , INPUT ) ;
+pinMode ( led , OUTPUT );
+}
+void loop() {
+if (analogRead(sensor) > 25  )  
+digitalWrite ( led , HIGH );
+else 
+digitalWrite ( led , LOW );
+}
